23621c1a230adf7a2b29686ea31db5b377facd74
hvelarde <hector.velarde@gmail.com>
2011-11-24 19:17:57 -0600

    reorganized egg to include development buildout

--- setup.py
+++ setup.py
@@ -14,27 +14,25 @@ setup(name='collective.newsflash',
         "Framework :: Plone",
         "Programming Language :: Python",
         ],
-      keywords='',
+      keywords='plone',
       author='',
       author_email='',
-      url='http://svn.plone.org/svn/collective/',
+      url='https://github.com/collective/collective.newsflash',
       license='GPL',
-      packages=find_packages(exclude=['ez_setup']),
+      packages=find_packages('src'),
+      package_dir={'': 'src'},
       namespace_packages=['collective'],
       include_package_data=True,
       zip_safe=False,
       install_requires=[
-          'setuptools',
-          'plone.app.z3cform',
-          'five.grok',
-          # -*- Extra requirements: -*-
-      ],
+        'setuptools',
+        'five.grok',
+        'plone.app.z3cform',
+        ],
       extras_require={
         'test': ['plone.app.testing'],
         },
       entry_points="""
-      # -*- Entry points: -*-
-
       [z3c.autoinclude.plugin]
       target = plone
       """,
