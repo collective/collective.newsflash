025ff876383888b375dfd7cf7b06091793de9371
hvelarde <hector.velarde@gmail.com>
2012-01-16 21:22:54 -0600

    renamed permission

--- src/collective/newsflash/tests/test_setup.py
+++ src/collective/newsflash/tests/test_setup.py
@@ -27,6 +27,12 @@ class InstallTest(unittest.TestCase):
         qi = getattr(self.portal, 'portal_quickinstaller')
         self.assertTrue(qi.isProductInstalled(PROJECTNAME))
 
+    def test_add_permission(self):
+        permission = 'collective.newsflash: Add News Flash'
+        roles = self.portal.rolesOfPermission(permission)
+        roles = [r['name'] for r in roles if r['selected']]
+        self.assertEqual(roles, ['Contributor', 'Manager', 'Owner', 'Site Administrator'])
+
     def test_browserlayer_installed(self):
         layers = [l.getName() for l in registered_layers()]
         self.assertTrue('INewsFlashLayer' in layers,
