9cbe6ef58d4a1011b2656bf720ab742a595f1d7c
hvelarde <hector.velarde@gmail.com>
2012-01-16 22:30:29 -0600

    refactored uninstall registry tests

--- src/collective/newsflash/tests/test_setup.py
+++ src/collective/newsflash/tests/test_setup.py
@@ -2,13 +2,10 @@
 
 import unittest2 as unittest
 
-from zope.component import getUtility
-
 from plone.app.testing import TEST_USER_ID
 from plone.app.testing import setRoles
 
 from plone.browserlayer.utils import registered_layers
-from plone.registry.interfaces import IRegistry
 
 from collective.newsflash.config import PROJECTNAME
 from collective.newsflash.testing import INTEGRATION_TESTING
@@ -51,7 +48,6 @@ class UninstallTest(unittest.TestCase):
     def setUp(self):
         self.portal = self.layer['portal']
         setRoles(self.portal, TEST_USER_ID, ['Manager'])
-        self.registry = getUtility(IRegistry)
         self.qi = getattr(self.portal, 'portal_quickinstaller')
         self.qi.uninstallProducts(products=[PROJECTNAME])
 
@@ -59,7 +55,6 @@ class UninstallTest(unittest.TestCase):
         self.assertTrue(not self.qi.isProductInstalled(PROJECTNAME))
 
     def test_browserlayer_removed(self):
-        # XXX: removal is implemented until plone.browserlayer 2.1.1
         layers = [l.getName() for l in registered_layers()]
         self.assertFalse('INewsFlashLayer' in layers,
                          'browser layer not removed')
@@ -68,17 +63,6 @@ class UninstallTest(unittest.TestCase):
         portal_javascripts = self.portal.portal_javascripts
         self.assertFalse(JS in portal_javascripts.getResourceIds(),
                          '%s not removed' % JS)
-
-    def test_records_removed_from_registry(self):
-        records = [
-            'collective.newsflash.controlpanel.INewsFlashSettings.controls',
-            'collective.newsflash.controlpanel.INewsFlashSettings.pauseOnItems',
-            'collective.newsflash.controlpanel.INewsFlashSettings.speed',
-            'collective.newsflash.controlpanel.INewsFlashSettings.titleText',
-            ]
-        for r in records:
-            self.assertTrue(r not in self.registry.records,
-                            '%s not removed from configuration registry' % r)
 
 
 def test_suite():
