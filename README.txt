5f0e57a97e6f0e19536802c875fdbc50e711a103
hvelarde <hector.velarde@gmail.com>
2012-07-12 19:21:06 -0300

    fixed package distribution, documentation and buildout development configurations

---
+++ README.txt
@@ -0,0 +1,30 @@
+====================
+collective.newsflash
+====================
+
+.. contents:: Table of Contents
+
+Overview
+--------
+
+News ticker inspired by the one on the `BBC News`_ website.
+
+Features
+--------
+
+- Lightweight and easy to use news ticker
+
+Use
+---
+
+- Open 'News Ticker Settings' configlet on Plone's 'Site Setup'.
+
+- Select the collection containing the items you want to display.
+
+That's it! You will see the items included in the collection on a site-wide
+news ticker.
+
+You can click on any item to open it.
+
+.. _`jQuery News Ticker`: http://www.jquerynewsticker.com/
+
--- setup.py
+++ setup.py
@@ -1,24 +1,30 @@
 # -*- coding: utf-8 -*-
 
-from setuptools import setup, find_packages
 import os
+from setuptools import setup, find_packages
 
 version = '1.0'
+long_description = open("README.txt").read() + "\n" + \
+                   open(os.path.join("docs", "INSTALL.txt")).read() + "\n" + \
+                   open(os.path.join("docs", "CREDITS.txt")).read() + "\n" + \
+                   open(os.path.join("docs", "HISTORY.txt")).read()
 
 setup(name='collective.newsflash',
       version=version,
-      description="An alternative implementation of the jQuery News Ticker Plugin for Plone.",
-      long_description=open("README.rst").read() + "\n" +
-                       open(os.path.join("docs", "INSTALL.txt")).read() + "\n" +
-                       open(os.path.join("docs", "CREDITS.txt")).read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      description="News ticker inspired by the one on the BBC News website.",
+      long_description=long_description,
       classifiers=[
         "Development Status :: 5 - Production/Stable",
+        "Environment :: Web Environment",
+        "Framework :: Plone",
         "Framework :: Plone :: 4.1",
+        "Framework :: Plone :: 4.2",
+        "Intended Audience :: System Administrators",
         "License :: OSI Approved :: GNU General Public License (GPL)",
         "Operating System :: OS Independent",
         "Programming Language :: JavaScript",
         "Programming Language :: Python",
+        "Programming Language :: Python :: 2.6",
         "Topic :: Office/Business :: News/Diary",
         "Topic :: Software Development :: Libraries :: Python Modules",
         ],
