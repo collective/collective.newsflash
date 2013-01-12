048e1e830ee2f53d65b5166232867c833808db93
quimera <s.huens@gmail.com>
2013-01-11 22:10:39 -0300

    init rte event in the prooverlay handler

--- src/collective/newsflash/browser/javascript/newsflash.js
+++ src/collective/newsflash/browser/javascript/newsflash.js
@@ -1,11 +1,23 @@
 jQuery(document).ready(function($) {  
     if ($('#edit-newsflashes')[0] !== undefined){
-        $('.manage-newsflashes').prepOverlay(
-            {
+        $('.manage-newsflashes').prepOverlay({
                 subtype: 'ajax',
                 formselector: '#form',
-                noform: 'reload'
-            }
-        );
+                noform: 'reload',
+                config: {
+                    onLoad : function (e) {
+                        if(typeof init_rte == 'function') {
+                            init_rte();
+                        }
+                        
+                    }
+                },
+                afterpost: function(response, data_parent) {
+                    if(typeof init_rte == 'function') {
+                        init_rte();
+                    }
+                    return true;
+                }
+            });
     }
 });
