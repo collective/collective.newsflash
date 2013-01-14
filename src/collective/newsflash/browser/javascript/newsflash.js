86ffc05f2c44254bdf34db383efc00542be2f64f
quimera <s.huens@gmail.com>
2013-01-14 15:18:08 -0300

    fixinig rte initialization, and prepoverlay binding

--- src/collective/newsflash/browser/javascript/newsflash.js
+++ src/collective/newsflash/browser/javascript/newsflash.js
@@ -13,11 +13,30 @@ jQuery(document).ready(function($) {
                     }
                 },
                 afterpost: function(response, data_parent) {
-                    if(typeof init_rte == 'function') {
-                        init_rte();
-                    }
+                    //XXX CUSTOM INIT_RTE                    
+                    $.each(response.find(".rte-widget"), function(){
+                        var $this = $(this);
+                        $(this).rte({
+                            content_css_url: "++resource++collective.z3cform.widgets/rte.css",
+                            media_url: "++resource++collective.z3cform.widgets/rte/",
+                            iframe_height: $this.data('iframe_height'),
+                            format_block: $this.data('format_block'),
+                            bold: $this.data('bold'),
+                            italic: $this.data('italic'),
+                            unordered_list: $this.data('unordered_list'),
+                            link: $this.data('link'),
+                            image: $this.data('image'),
+                            allow_disable: $this.data('allow_disable')
+                        });
+                    });
+
+
                     return true;
                 }
             });
+        $('body').delegate('#add-newsflashes', 'click', function(e){
+            e.preventDefault();
+            $('#edit-newsflashes').click();
+        });
     }
 });
